5bd579ac540596802ac3c9c95dbab3f9ac2494ba
fazalshahzad <shancompany322@gmail.com>
2022-06-08 11:59:46 +0500

    commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { BlogSingleComponent } from './blog-single/blog-single.component';
+import { BlogComponent } from './blog/blog.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { ReserveComponent } from './reserve/reserve.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'',component:HomeComponent},
+{path:'home',component:HomeComponent},
+{path:'about',component:AboutComponent},
+{path:'blog',component:BlogComponent},
+{path:'single',component:BlogSingleComponent},
+{path:'menu',component:MenuComponent},
+{path:'contact',component:ContactComponent},
+{path:'reserve',component:ReserveComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BlogComponent } from './blog/blog.component';
+import { BlogSingleComponent } from './blog-single/blog-single.component';
+import { ContactComponent } from './contact/contact.component';
+import { MenuComponent } from './menu/menu.component';
+import { ReserveComponent } from './reserve/reserve.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    BlogSingleComponent,
+    ContactComponent,
+    MenuComponent,
+    ReserveComponent
   ],
   imports: [
     BrowserModule,
